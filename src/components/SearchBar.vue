a1faece6eb19f257c40890fe48c580852a9e59a9
Stuart Harper <stuart.harper@stgconsulting.com>
2020-12-30 07:29:01 -0700

    Search - add vuex method and base components. Still in progress

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <form id="searchBar" @submit="submitSearch">
+    <input
+      id="search"
+      v-model="searchString"
+      type="text"
+      name="search" 
+    />
+    <input type="submit" style="display: none" />
+    <button type="submit" form="searchBar" value="Submit">Search</button>
+  </form>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    name: 'Search Bar',
+    data() {
+      return {
+        searchString: ""  
+      }
+    },
+    methods: {
+      ...mapActions([ 'getSearchResults' ]),
+      submitSearch: function () {
+        this.getSearchResults(this.searchString);
+        this.searchString = '';
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
--- src/store.js
+++ src/store.js
@@ -1,17 +1,22 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import 'es6-promise/auto';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    isAuthenticated: false
+    isAuthenticated: false,
+    searchResults: []
   },
   mutations: {
     setIsAuthenticated: (state, payload) => {
-      state.isAuthenticated = payload
-    }
+      state.isAuthenticated = payload;
+    },
+    setSearchResults: (state, payload) => {
+      state.searchResults = payload;
+    },
   },
   actions: {
     login: ({commit}) => {
@@ -19,6 +24,14 @@ const store = new Vuex.Store({
     },
     logout: ({commit}) => {
       commit('setIsAuthenticated', false);
+    },
+    getSearchResults: async ({commit}, searchString) => {
+      try {
+        let result = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${encodeURI(searchString)}&key=[YOUR_API_KEY]`);
+        commit('setSearchResults', result.data);
+      } catch (e) {
+        console.error(e);
+      }
     }
   }
 });
