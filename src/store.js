5ff2c117ded846f79687129db6ab419bfdadfa64
Stuart Harper <stuart.harper@stgconsulting.com>
2021-01-04 08:56:13 -0700

    favorites - update vuex to use favorites

--- src/store.js
+++ src/store.js
@@ -8,7 +8,9 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {
     isAuthenticated: false,
-    searchResults: []
+    searchResults: [],
+    favoriteVideos: [],
+    favoriteVideoIds: []
   },
   mutations: {
     setIsAuthenticated: (state, payload) => {
@@ -16,6 +18,16 @@ const store = new Vuex.Store({
     },
     setSearchResults: (state, payload) => {
       state.searchResults = payload;
+    },
+    setFavoriteVideos: (state, payload) => {
+      state.favoriteVideos = payload;
+    },
+    setFavoriteVideoIds: (state, payload) => {
+      if (payload.addFavorite) {
+        state.favoriteVideoIds = [payload.videoId, ...state.favoriteVideoIds];
+      } else {
+        state.favoriteVideoIds = state.favoriteVideoIds.filter(id => id !== payload.videoId);
+      }
     },
   },
   actions: {
@@ -29,6 +41,26 @@ const store = new Vuex.Store({
       try {
         let result = await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${encodeURI(searchString)}&key=AIzaSyCrmoOmzDXuHcGS9qCzvO3JHiXYGKe0AKs`);
         commit('setSearchResults', result?.data?.items);
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    addFavorite: ({commit}, videoId) => {
+      commit('setFavoriteVideoIds', {addFavorite: true, videoId});
+    },
+    removeFavorite: ({commit}, videoId) => {
+      commit('setFavoriteVideoIds', {addFavorite: false, videoId});
+    },
+    getFavoriteVideos: async ({commit, state}) => {
+      try {
+        let result = await axios.get(
+          `https://www.googleapis.com/youtube/v3/search?part=snippet&id=${
+            encodeURI(
+              state.favoriteVideoIds.reduce((idString, id) => idString.length ? idString + ',' + id : id, '')
+            )
+          }&key=AIzaSyCrmoOmzDXuHcGS9qCzvO3JHiXYGKe0AKs`
+        );
+        commit('setFavoriteVideos', result?.data?.items);
       } catch (e) {
         console.error(e);
       }
